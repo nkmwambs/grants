cfc3222982a3e23eb50f8bcd2dc530f90fa00c0c
nkmwambs <nkmwambs@gmail.com>
2019-10-29 13:13:17 +0300

    Working on ACL

---
+++ application/config/rbac.php
@@ -0,0 +1,16 @@
+<?php
+
+// Request related permissions
+$config['permissions']['add_request'] = "Add a new request";
+$config['permissions']['edit_request'] = "Edit a request";
+$config['permissions']['delete_request'] = "Delete a request";
+$config['permissions']['view_request'] = "View a new request";
+$config['permissions']['approve_request'] = "Approve a request";
+$config['permissions']['escalate_request'] = "Escalate a request to a voucher";
+
+// System Roles
+$config['roles']['authenticated_user'] = "Authenticated User";
+$config['roles']['accountant'] = "Accountant";
+$config['roles']['finance_director'] = "Finance Director";
+$config['roles']['super_admin'] = "Supper Administrator";
+$config['roles']['system_admin'] = "System Administrator";
