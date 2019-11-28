9b7a4b049c8e9d733547a42818c4184b7fccef8a
nkmwambs <nkmwambs@gmail.com>
2019-11-28 09:18:44 +0300

    Resolving View_output error

--- application/third_party/Output/View/View_output.php
+++ application/third_party/Output/View/View_output.php
@@ -513,4 +513,4 @@ function detail_list_output(String $table): Array {
 
 }
 
-require_once(__DIR__.DIRECTORY_SEPARATOR.'..\create_instance.php');
+require_once(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'create_instance.php');
