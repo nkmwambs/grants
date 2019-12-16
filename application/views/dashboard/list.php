5c3343ef1c4bdd36972f76e69a36ebc30e3ff863
nkmwambs <nkmwambs@gmail.com>
2019-12-16 16:16:23 +0300

    Permission label depth now applies. Development done

--- application/views/dashboard/list.php
+++ application/views/dashboard/list.php
@@ -1,8 +1,6 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-//print_r($this->session->departments);
-//$this->db->where_in('fk_department_id',$this->session->departments);
-//print_r($this->db->get('department')->result_array()); 
+//print_r($this->user_model->permission_label_depth('update'));
 
 ?>
 <link rel="stylesheet" href="<?=base_url();?>assets/trial/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css">
