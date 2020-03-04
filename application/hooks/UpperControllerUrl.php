74f618746e1a61aa4f8393ea52e60b654c85dc2a
nkmwambs <nkmwambs@gmail.com>
2020-03-04 15:46:33 +0300

    Added a URL modifier via a hook - 2

--- application/hooks/UpperControllerUrl.php
+++ application/hooks/UpperControllerUrl.php
@@ -2,6 +2,28 @@
 
 class UpperControllerUrl{
     public function run(){
-        $_SERVER['REQUEST_URI'] = ucfirst($_SERVER['REQUEST_URI']);
+        if($_SERVER['HTTP_HOST'] !== 'localhost'){
+            $_SERVER['REQUEST_URI'] = $this->cap_url_controller($_SERVER['REQUEST_URI']);
+        }
+    }
+
+    function cap_url_controller($url){
+		$url_segments = parse_url($url);
+
+		$path_array = explode("/",ltrim($url_segments['path'],'/'));
+
+		array_shift($path_array);
+
+		$arguments_array = array_map(function($segment,$index){
+
+		if($index == 0){
+			return ucfirst($segment);
+		}else{
+			return $segment;
+		}
+
+		},$path_array,array_keys($path_array));
+
+		echo implode("/",$arguments_array);
     }
 }
