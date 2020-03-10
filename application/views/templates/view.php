322f9db037bc5dd2735f8c85384d0d31503d3513
nkmwambs <nkmwambs@gmail.com>
2020-03-10 20:59:10 +0300

    Edit content of view file - Commmented grants methods

--- application/views/templates/view.php
+++ application/views/templates/view.php
@@ -1,6 +1,6 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-$action_labels = $this->grants->action_labels($this->controller,hash_id($this->id,'decode'));
+//$action_labels = $this->grants->action_labels($this->controller,hash_id($this->id,'decode'));
 
 //print_r($this->grants->get_record_office_id('budget_item',22));
 
@@ -11,6 +11,6 @@ extract($result['master']);
 //$this->grants->unset_lookup_tables_ids($keys);
 
 // Make the master detail table have columns as per the config
-$columns = array_chunk($keys,$this->config->item('master_table_columns'),true);
+//$columns = array_chunk($keys,$this->config->item('master_table_columns'),true);
 
 ?>
