adc1368d0af083fc351c232f5388dfac044fcee0
nkmwambs <nkmwambs@gmail.com>
2020-03-10 17:42:32 +0300

    Use of capped controller in list view - 5

--- application/views/templates/single_form_add.php
+++ application/views/templates/single_form_add.php
@@ -1,6 +1,7 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-  print_r($this->grants_model->lookup_values($this->controller));
+  print_r($this->grants_model->lookup_values_keys($this->controller));
+  print_r($this->grants_model->lookup_values_values($this->controller));
 
   extract($result);
   //echo isset($this->session->master_table)?$this->session->master_table:"Not set";
