b9fc68c9771d145644266f97327f17e762149703
nkmwambs <nkmwambs@gmail.com>
2020-03-10 21:46:47 +0300

    Mimic ['show_label_as_button'] result - 11

--- application/libraries/autoloaded/Grants.php
+++ application/libraries/autoloaded/Grants.php
@@ -1580,7 +1580,7 @@ function action_labels($table,$primary_key){
   //$this->load_detail_model('approval');
   //$this->CI->load->model('approval');
 
-  return $this->CI->grants_model->display_approver_status_action($this->CI->session->role_id, $table, $primary_key);
+  return $this->CI->grants_model->action_labels($table, $primary_key);
 
 }
 
