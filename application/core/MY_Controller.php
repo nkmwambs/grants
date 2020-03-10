621cb23c22484eec057637517a9122045c56e8f3
nkmwambs <nkmwambs@gmail.com>
2020-03-10 16:31:01 +0300

    Use require once to load packages in My Controller

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -4,6 +4,8 @@
   The third_party houses the reusable API or code systemwise
  */
 require_once APPPATH."third_party/Api/autoload.php";
+require_once APPPATH.'third_party'.DIRECTORY_SEPARATOR.'Packages'.DIRECTORY_SEPARATOR.'Core';
+require_once APPPATH.'third_party'.DIRECTORY_SEPARATOR.'Packages'.DIRECTORY_SEPARATOR.'Grants';
 
 class MY_Controller extends CI_Controller implements CrudModelInterface
 {
