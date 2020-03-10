f87918f002381c24d1f76b0848fb18e60710e432
nkmwambs <nkmwambs@gmail.com>
2020-03-10 22:24:49 +0300

    Moved Approval model methods for approval to General model - 1

--- application/views/templates/view.php
+++ application/views/templates/view.php
@@ -1,7 +1,7 @@
 <?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 //$action_labels['show_label_as_button'] = false;//$this->grants->action_labels($this->controller,hash_id($this->id,'decode'));
-print_r($this->general_model->test());
+//print_r($this->general_model->test());
 
 extract($result['master']);
 
