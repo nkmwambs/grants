151ff5758362a1bde7cda190f4eac77a82ee5282
nkmwambs <nkmwambs@gmail.com>
2020-03-18 11:08:14 +0300

    added an extra route without uri segment 3 e.g. login - 3

--- application/config/routes.php
+++ application/config/routes.php
@@ -67,7 +67,9 @@ if($uri[2] && isset($uri[3]))
     $route[strtolower($uri[2]).'/'.$uri[3]] = ucfirst($uri[2]).'/'.$uri[3];
     $route[strtolower($uri[2]).'/'.$uri[3].'/:any'] = ucfirst($uri[2]).'/'.$uri[3].'/$1';
    
-} 
+}elseif($uri[2]){
+    $route[strtolower($uri[2])] = ucfirst($uri[2]);
+}
 
 
 spl_autoload_register(function($classname){
