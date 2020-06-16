86606035f547b7e754628acae3dbcea7a27d8a5f
nkmwambs <nkmwambs@gmail.com>
2020-06-16 07:32:27 +0300

    Added a project allocation as detail of income account

--- application/third_party/Packages/Grants/models/Income_account_model.php
+++ application/third_party/Packages/Grants/models/Income_account_model.php
@@ -33,7 +33,7 @@ class Income_account_model extends MY_Model implements CrudModelInterface, Table
   }
 
   function detail_tables(){
-    return ['expense_account'];
+    return ['expense_account','project_allocation'];
   }
 
   function list(){
