4572b88ad1e1973b27d27617317923adadc1ec68
nkmwambs <nkmwambs@gmail.com>
2020-07-03 08:31:28 +0300

    Added a print button to financial report

--- application/views/financial_report/ajax_view.php
+++ application/views/financial_report/ajax_view.php
@@ -7,77 +7,82 @@
 //$expense_account_month_expense = array_column(array_column(array_column($expense_report,'income_account'),'expense_accounts'),'month_expense');
 
 ?>
-<div class="row">
-    <div class='col-xs-12 header'><?=get_phrase('fund_balance_report');?></div>
-    <div class="col-xs-12">
-        <?php include "includes/include_fund_balance_report.php";?>
-    </div>
-</div>
-
-<!-- <div class='row'>
-    <div class='col-xs-12 header'><?=get_phrase('projects_balance_report');?></div>
-    <div class='col-xs-12'>
-        <?php //include "includes/include_project_balance_report.php";?>
-    </div>
-</div> -->
-
-<div class="row">
-    <div class="col-xs-6">
-    <div class="col-xs-12 header"><?=get_phrase('proof_of_cash');?></div>
-        <?php include "includes/include_proof_of_cash.php";?>
+<div id="voucher_print">
+    <div class="row">
+        <div class='col-xs-12 header'><?=get_phrase('fund_balance_report');?></div>
+        <div class="col-xs-12">
+            <?php include "includes/include_fund_balance_report.php";?>
+        </div>
     </div>
 
-    <div class="col-xs-6">
-    <div class="col-xs-12 header"><?=get_phrase('financial_ratios');?></div>
-        <?php //include "includes/include_financial_ratios.php";?>
+    <!-- <div class='row'>
+        <div class='col-xs-12 header'><?=get_phrase('projects_balance_report');?></div>
+        <div class='col-xs-12'>
+            <?php //include "includes/include_project_balance_report.php";?>
+        </div>
+    </div> -->
+
+    <div class="row">
+        <div class="col-xs-6">
+        <div class="col-xs-12 header"><?=get_phrase('proof_of_cash');?></div>
+            <?php include "includes/include_proof_of_cash.php";?>
+        </div>
+
+        <div class="col-xs-6">
+        <div class="col-xs-12 header">
+        <?php //echo get_phrase('financial_ratios');?>
+        </div>
+            <?php //include "includes/include_financial_ratios.php";?>
+        </div>
+
     </div>
 
-</div>
+    <div class="row">
+        <div class="col-xs-6">
+        <div class="col-xs-12 header"><?=get_phrase('bank_reconciliation');?></div>
+            <?php include "includes/include_bank_reconciliation.php";?>    
+        </div>
 
-<div class="row">
-    <div class="col-xs-6">
-    <div class="col-xs-12 header"><?=get_phrase('bank_reconciliation');?></div>
-        <?php include "includes/include_bank_reconciliation.php";?>    
+        <div class="col-xs-6">
+            <div class="col-xs-12 header"><?=get_phrase('bank_statements');?></div>
+            <?php include "includes/include_bank_statements.php";?>
+        </div>
     </div>
 
-    <div class="col-xs-6">
-        <div class="col-xs-12 header"><?=get_phrase('bank_statements');?></div>
-        <?php include "includes/include_bank_statements.php";?>
-    </div>
-</div>
+    <div class="row">
+        <div class="col-xs-6">
+        <div class="col-xs-12 header"><?=get_phrase('outstanding_cheques');?></div>
+            <?php include "includes/include_outstanding_cheques.php";?>
+        </div>
 
-<div class="row">
-    <div class="col-xs-6">
-    <div class="col-xs-12 header"><?=get_phrase('outstanding_cheques');?></div>
-        <?php include "includes/include_outstanding_cheques.php";?>
-    </div>
+        <div class="col-xs-6">
+        <div class="col-xs-12 header"><?=get_phrase('outstanding_cheques_cleared_effects');?></div>
+            <?php include "includes/include_cleared_outstanding_cheques.php";?>
+        </div>
 
-    <div class="col-xs-6">
-    <div class="col-xs-12 header"><?=get_phrase('outstanding_cheques_cleared_effects');?></div>
-        <?php include "includes/include_cleared_outstanding_cheques.php";?>
     </div>
 
-</div>
 
+    <div class="row">
+        <div class="col-xs-6">
+        <div class="col-xs-12 header"><?=get_phrase('deposit_in_transit');?></div>
+            <?php include "includes/include_deposit_in_transit.php";?>
+        </div>
 
-<div class="row">
-    <div class="col-xs-6">
-    <div class="col-xs-12 header"><?=get_phrase('deposit_in_transit');?></div>
-        <?php include "includes/include_deposit_in_transit.php";?>
+        <div class="col-xs-6">
+        <div class="col-xs-12 header"><?=get_phrase('deposit_in_transit_cleared_effects');?></div>
+            <?php include "includes/include_cleared_deposit_in_transit.php";?>
+        </div>
+
     </div>
 
-    <div class="col-xs-6">
-    <div class="col-xs-12 header"><?=get_phrase('deposit_in_transit_cleared_effects');?></div>
-        <?php include "includes/include_cleared_deposit_in_transit.php";?>
+    <div class="row">
+        <div class="col-xs-12">
+        <div class="col-xs-12 header"><?=get_phrase('expense_report');?></div>
+            <?php include "includes/include_expense_report.php";?>
+        </div>
     </div>
 
-</div>
-
-<div class="row">
-    <div class="col-xs-12">
-    <div class="col-xs-12 header"><?=get_phrase('expense_report');?></div>
-        <?php include "includes/include_expense_report.php";?>
-    </div>
 </div>
 
 <hr/>
