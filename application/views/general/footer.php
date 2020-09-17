dc7e204656d4895e2059e2647f4d6d4bead93d67
Nicodemus Karisa <nkarisa@ke.ci.org>
2020-09-17 22:09:51 +0300

    Updated footer address

--- application/views/general/footer.php
+++ application/views/general/footer.php
@@ -2,7 +2,7 @@
 <footer class="main">
 	&copy; <?=date('Y');?> <strong><?php echo $system_name;?></strong>.
     Developed by
-	<a href="https://compassion.com"
+	<a href="https://www.compassion.com"
     	target="_blank">Compassion International Inc</a>
 
 
