88b49dda12b296f02e04ec58b38004ff4ba835ac
Nicodemus Karisa <nkarisa@ke.ci.org>
2020-11-26 15:12:13 +0300

    Clear button labels - 3

--- application/views/financial_report/includes/include_cleared_outstanding_cheques.php
+++ application/views/financial_report/includes/include_cleared_outstanding_cheques.php
@@ -15,12 +15,12 @@
                             $clear_outstanding_cheque_state_color = "danger";
                             //$oustanding_state_disabled = "";
                             $clear_outstanding_cheque_state_clear_class = 'to_clear';
-                            $clear_outstanding_cheque_state_label = get_phrase('clear');
+                            $clear_outstanding_cheque_state_label = get_phrase('unclear');
                             if($clear_outstanding_cheque['voucher_cleared'] == 1){
                                 $clear_outstanding_cheque_state_color = "success";
                                 //$oustanding_state_disabled = "disabled";
                                 //$oustanding_state_clear_class = '';
-                                $clear_outstanding_cheque_state_label = get_phrase('unclear');
+                                //$clear_outstanding_cheque_state_label = get_phrase('unclear');
                             }
                         ?>
                         <td>
