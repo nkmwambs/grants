8a667d060a75d216bd8a8c627137be00a369638d
Nicodemus Karisa <nkarisa@ke.ci.org>
2020-12-08 16:47:30 +0300

    Prevent creating multiple default banks

--- application/controllers/Office_bank.php
+++ application/controllers/Office_bank.php
@@ -21,6 +21,22 @@ class Office_bank extends MY_Controller
 
   function index(){}
 
+  function check_office_has_default_office_bank(){
+    $office_has_default_office_bank = false;
+    $post = $this->input->post();
+
+    $this->read_db->where(array('fk_office_id'=>$post['office_id'],'office_bank_is_active'=>1,
+    'office_bank_is_default'=>1));
+    $active_default_office_bank = $this->read_db->get('office_bank');
+
+    if($active_default_office_bank->num_rows() > 0){
+      $office_has_default_office_bank = true;
+    }
+
+    echo $office_has_default_office_bank;
+
+  }
+
   static function get_menu_list(){}
 
 }
