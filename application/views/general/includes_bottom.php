d8232dd3d5ed339a1301bbb4318d45025efb5c03
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-11 13:41:23 +0300

    Updated font Awesome CDN

--- application/views/general/includes_bottom.php
+++ application/views/general/includes_bottom.php
@@ -32,7 +32,7 @@
 	
 	<!--Font Awesome-->
 	
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" /> -->
 	
 	
 		<!-- Monkey Modal Dialog  CSS / JS-->
