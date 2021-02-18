099e0c7683fa47599f3e40dd4f358802fc987552
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-18 13:00:56 +0300

    Working on new S3 Lib

---
+++ application/config/Aws_attachment.php
@@ -0,0 +1,11 @@
+<?php
+
+	// S3 Configuration
+	$config['upload_files_to_s3'] = true;
+	$config['s3_region'] = 'eu-west-1';
+	$config['s3_bucket_name'] = 'compassion-fcp-fms-version2';
+	$config['temp_files_deletion_limit_hours'] = 0.5; // In hours. Use fractional of while or whole number e.g. 1, 2, 2.5
+
+    $config['attachment_table_name'] = 'attachment';
+    $config['write_db_array_connection_key'] = 'write_db';
+    $config['read_db_array_connection_key'] = 'read_db';
