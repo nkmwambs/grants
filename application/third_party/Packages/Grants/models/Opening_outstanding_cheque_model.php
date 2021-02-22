b7a1faac73db2757c5c9e701fedfb6fa0377539e
Nicodemus Karisa <nkarisa@ke.ci.org>
2021-02-22 17:36:27 +0300

    Control visibility of opening balances by office hierarchy

--- application/third_party/Packages/Grants/models/Opening_outstanding_cheque_model.php
+++ application/third_party/Packages/Grants/models/Opening_outstanding_cheque_model.php
@@ -94,4 +94,11 @@ class Opening_outstanding_cheque_model extends MY_Model{
         return $lookup_values;
         
     }
+
+    function list_table_where(){
+        if(!$this->session->system_admin){
+            $office_ids = array_column($this->session->hierarchy_offices,'office_id');
+            $this->db->where_in('system_opening_balance.fk_office_id',$office_ids);
+        }
+      }
 }
